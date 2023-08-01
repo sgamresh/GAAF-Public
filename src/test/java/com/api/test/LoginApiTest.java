b4bc510a6b8d83096636e505e0ca3784793a1335
Amresh <amresh.pradhan@viacom18.com>
2023-08-01 09:26:21 +0530

    Refactoring

--- src/test/java/com/api/test/LoginApiTest.java
+++ src/test/java/com/api/test/LoginApiTest.java
@@ -11,7 +11,7 @@ import io.restassured.response.Response;
 public class LoginApiTest extends BaseTest{
 	
 
-	@Test
+	@Test(invocationCount = 1)
 	public void LoginApiTestCases1() {
 		 String BASE_URL;
 		 Response res;
@@ -19,9 +19,10 @@ public class LoginApiTest extends BaseTest{
 			BASE_URL = config.api_joke();
 			 event.log("INFO", "BASE_URL"+BASE_URL);
 
-			   res=  (Response) RestAssured.given().get(BASE_URL); // Assert the response status code is 200 (OK).log().all();
-		
-				 event.log("INFO",  res);
+			   for (int i = 0; i < 10; i++) {
+				res = (Response) RestAssured.given().get(BASE_URL); // Assert the response status code is 200 (OK).log().all();
+				event.log("INFO", res);
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
